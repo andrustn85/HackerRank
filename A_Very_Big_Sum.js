126920ba5f54c6c426f476f3b14eee5d4d34683c
sarah <sarahkharraz@gmail.com>
2020-03-20 21:34:30 +0000

    A very big sum - Solution

--- A_Very_Big_Sum.js
+++ A_Very_Big_Sum.js
@@ -30,3 +30,52 @@
 //
 // The range of the 32-bit integer is .
 // When we add several integer values, the resulting sum might exceed the above range. You might need to use long long int in C/C++ or long data type in Java to store such sums.
+
+
+// SOLUTION
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the aVeryBigSum function below.
+function aVeryBigSum(ar) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+return ar.reduce(reducer)
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = aVeryBigSum(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
