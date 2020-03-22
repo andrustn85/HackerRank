40d95aa4fcb4db084e7593e336472bf083c51349
sarah <sarahkharraz@gmail.com>
2020-03-22 13:59:45 +0000

    Sample test exercise : Sort the input

--- Odd_in_a_Range.js
+++ Odd_in_a_Range.js
@@ -1,7 +1,5 @@
 // Exercise = Sample test for interviews
-//
 // Given two numbers `left` and `right` which are the begining and the end of an array.
-//
 // Return all the odd numbers in between including the two given numbers
 
 
---
+++ Sort_Vowels_and_Consonants.js
@@ -0,0 +1,6 @@
+// Exercise = Sample test for interviews
+
+// Given an input string
+// Return all the vowels in the same order as the input
+// Return all the consonants in the same order as the input
+// Final output has to return the vowels following by the consonants in an array
