aa37383225e0231a3200a82fbbdce3574dc20ce3
sarah <sarahkharraz@gmail.com>
2020-03-22 13:09:58 +0000

    Solution of find the odd

--- Odd_in_a_Range.js
+++ Odd_in_a_Range.js
@@ -3,3 +3,25 @@
 // Given two numbers `left` and `right` which are the begining and the end of an array.
 //
 // Return all the odd numbers in between including the two given numbers
+
+
+// SOLUTION (not verified via HackerRank)
+
+const left = 3
+const right = 9
+
+// give all the numbers in between
+const allNum = [];
+for (let i = left; i <= right; i++) {
+  allNum.push(i);
+}
+console.log('all numbers', allNum)
+
+// give all the odd number in between
+const oddNum = []
+for (let od = allNum[0]; od <= allNum[allNum.length-1]; od++) {
+  if(od % 2 !== 0){
+    oddNum.push(od);
+  }
+  console.log('oddNum', oddNum)
+}
