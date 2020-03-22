ee96251f85d833d62fb082176380d7632d52e945
sarah <sarahkharraz@gmail.com>
2020-03-22 17:35:49 +0000

    FizzBuzz exercise

---
+++ FizzBuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(n){
+  for(let num = 1; num <= n; num++){
+    if(num % 15 === 0){ console.log('FizzBuzz')}
+    else if(num % 3 === 0){ console.log('Fizz')}
+    else if(num % 5 === 0){ console.log('Buzz')}
+    else { console.log(num)}
+  }
+}
+console.log(fizzBuzz(40))
