c9f6dc2c1a547039fec6fb401113bd3dd4cf3f95
sarah <sarahkharraz@gmail.com>
2020-03-25 18:46:24 +0000

    1_Function : Solution

--- 10DaysOfJavascript/1_Functions.js
+++ 10DaysOfJavascript/1_Functions.js
@@ -25,3 +25,49 @@
 // Explanation 0
 //
 // We return the value of .
+
+
+// SOLUTION
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+const factorial = n => {
+    if (n < 0)
+        return -1;
+    else if (n === 0)
+        return 1;
+    else {
+        const res = (n * factorial(n - 1))
+        return res;
+    }
+}
+
+function main() {
+    const n = +(readLine());
+
+    console.log(factorial(n));
+}
