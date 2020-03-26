b202a24e9b62c577be38511c52739d93bc7705aa
sarah <sarahkharraz@gmail.com>
2020-03-26 10:06:45 +0000

    10D - Regular Expression solution

--- 10DaysOfJavascript/7_Regular_Expressions_I.js
+++ 10DaysOfJavascript/7_Regular_Expressions_I.js
@@ -53,3 +53,51 @@
 // Explanation 3
 //
 // This string starts with the vowel a but ends in the vowel o.
+
+// SOLUTION
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+
+    const re = /^([aeiou]).+\1$/
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+
+    console.log(re.test(s));
+}
