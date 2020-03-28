948bce641d8eebe64335b338dca86416083aaf33
sarah <sarahkharraz@gmail.com>
2020-03-28 21:32:43 +0000

    10D - Arrays solutions

--- 10DaysOfJavascript/3_Arrays.js
+++ 10DaysOfJavascript/3_Arrays.js
@@ -30,3 +30,57 @@
 // Explanation 0
 //
 // Given the array , we see that the largest value in the array is  and the second largest value is . Thus, we return  as our answer.
+
+
+// SOLUTION
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+
+    const maxNum = Math.max(...nums)
+    const idx = nums.indexOf(maxNum)
+    const removeMax = nums.splice(idx, 1)
+
+    const removeAgain = nums.splice(idx, 1)
+    const includes = nums.includes(maxNum)? removeAgain : nums
+    const secondBigger = Math.max(...nums)
+
+    return secondBigger
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+
+    console.log(getSecondLargest(nums));
+}
