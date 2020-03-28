e56852590f281b7f3f2795787a25893dc294d9f6
sarah <sarahkharraz@gmail.com>
2020-03-28 10:00:19 +0000

    30D - Recusion 3 solution

--- 30DaysOfCode/9_Recursion_3.js
+++ 30DaysOfCode/9_Recursion_3.js
@@ -29,3 +29,57 @@
 // Consider the following steps:
 //
 // From steps  and , we can say ; then when we apply the value from  to step , we get . Thus, we print  as our answer.
+
+// SOLUTION
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the factorial function below.
+function factorial(n) {
+
+    if (n < 0)
+        return -1;
+    else if (n === 0)
+        return 1;
+    else {
+        const res = (n * factorial(n - 1))
+        return res;
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = factorial(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
