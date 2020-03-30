0392313a62a4e9f0eed5dcd1756f48442918a988
sarah <sarahkharraz@gmail.com>
2020-03-30 18:54:07 +0100

    10D - Throw solution

--- 10DaysOfJavascript/3_Throw.js
+++ 10DaysOfJavascript/3_Throw.js
@@ -70,3 +70,60 @@
 //
 // isPositive(-1): Because , we throw an Error with  Negative Error. This is caught by the locked stub code and the value of its  is printed.
 // isPositive(20): This returns YES because  is positive.
+
+// SOLUTION
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    try {
+        if(a > 0){throw 'YES'}
+        if(a === 0){throw "Zero Error"}
+        if(a < 0){throw "Negative Error"}
+    } catch(error){
+      return error
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+
+    for (let i = 0; i < n; i++) {
+        const a = +(readLine());
+
+        try {
+            console.log(isPositive(a));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
