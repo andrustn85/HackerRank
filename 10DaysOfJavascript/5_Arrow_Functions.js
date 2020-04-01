ec9504f89f4e962f8df331534db7b7d52f4404f1
sarah <sarahkharraz@gmail.com>
2020-04-01 20:39:52 +0100

    10D - Arrow functions - solution

--- 10DaysOfJavascript/5_Arrow_Functions.js
+++ 10DaysOfJavascript/5_Arrow_Functions.js
@@ -32,3 +32,57 @@
 // Explanation 0
 //
 // Given , we modify each element so that all even elements are multiplied by  and all odd elements are multipled by . In other words, . We then return the modified array as our answer.
+
+// SOLUTION
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map(string => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+  const map = nums.map(item => (item % 2 === 0 ? item * 2 : item * 3));
+  return map;
+}
+
+function main() {
+  const n = +readLine();
+  const a = readLine()
+    .split(" ")
+    .map(Number);
+
+  console.log(
+    modifyArray(a)
+      .toString()
+      .split(",")
+      .join(" ")
+  );
+}
