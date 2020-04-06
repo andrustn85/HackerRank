1f2f838b5492bacf53bdb259bc668306bb93cad4
sarah <sarahkharraz@gmail.com>
2020-04-06 21:12:24 +0100

    10D - Classes - solution

--- 10DaysOfJavascript/4_Classes.js
+++ 10DaysOfJavascript/4_Classes.js
@@ -30,3 +30,29 @@
 // // Print the perimeter
 // console.log(triangle.perimeter());
 // When executed with a properly implemented Polygon class, this code should print the result of .
+
+// SOLUTION
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  perimeter() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return this.sides.reduce(reducer);
+  }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
