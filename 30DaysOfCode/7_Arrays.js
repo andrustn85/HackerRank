9a39af0d1eb1cca94035b16fb61320284f44bfd9
sarah <sarahkharraz@gmail.com>
2020-04-28 16:52:29 +0100

    30D - Arrays - solution

--- 30DaysOfCode/7_Arrays.js
+++ 30DaysOfCode/7_Arrays.js
@@ -23,3 +23,39 @@
 // Sample Output
 //
 // 2 3 4 1
+
+// SOLUTION
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map(str => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const n = parseInt(readLine(), 10);
+  const arr = readLine()
+    .split(" ")
+    .map(arrTemp => parseInt(arrTemp, 10));
+  const sort = arr.reverse().join(" ");
+  console.log(sort);
+}
