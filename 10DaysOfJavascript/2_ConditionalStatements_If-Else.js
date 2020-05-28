d11ffbdfab0f19c09924b074f87cad7b157d0515
sarah <sarahkharraz@gmail.com>
2020-05-28 17:58:01 +0100

    10D - refactoring

--- 10DaysOfJavascript/2_ConditionalStatements_If-Else.js
+++ 10DaysOfJavascript/2_ConditionalStatements_If-Else.js
@@ -1,81 +1,85 @@
-// Objective
-//
-// In this challenge, we learn about if-else statements. Check out the attached tutorial for more details.
-//
-// Task
-//
-// Complete the getGrade(score) function in the editor. It has one parameter: an integer, , denoting the number of points Julia earned on an exam. It must return the letter corresponding to her  according to the following rules:
-//
-// If , then .
-// If , then .
-// If , then .
-// If , then .
-// If , then .
-// If , then .
-// Input Format
-//
-// Stub code in the editor reads a single integer denoting  from stdin and passes it to the function.
-//
-// Constraints
-//
-// Output Format
-//
-// The function must return the value of  (i.e., the letter grade) that Julia earned on the exam.
-//
-// Sample Input 0
-//
-// 11
-// Sample Output 0
-//
-// D
-// Explanation 0
-//
-// Because , it satisfies the condition  (which corresponds to D). Thus, we return D as our answer.
+/*
 
+Objective
+In this challenge, we learn about if-else statements. Check out the attached tutorial for more details.
+
+Task
+Complete the getGrade(score) function in the editor. It has one parameter: an integer, , denoting the number of points Julia earned on an exam. It must return the letter corresponding to her  according to the following rules:
+
+If , then .
+If , then .
+If , then .
+If , then .
+If , then .
+If , then .
+
+Input Format
+Stub code in the editor reads a single integer denoting  from stdin and passes it to the function.
+
+Constraints
+Output Format
+The function must return the value of  (i.e., the letter grade) that Julia earned on the exam.
+
+Sample Input 0
+11
+Sample Output 0
+D
+Explanation 0
+Because , it satisfies the condition  (which corresponds to D). Thus, we return D as our answer.
+
+*/
 
 // SOLUTION
 
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map(string => {
+      return string.trim();
     });
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 function getGrade(score) {
-   let grade;
-    // Write your code here
+  let grade;
 
-        if( score > 25 ){ grade = 'A' }
-        else if( score > 20 ){ grade = 'B' }
-        else if( score > 15 ){ grade = 'C' }
-        else if( score > 10 ){ grade = 'D' }
-        else if( score > 5 ){ grade = 'E' }
-        else if( score >= 0 ){ grade = 'F' }
+  if (score > 25) {
+    grade = "A";
+  } else if (score > 20) {
+    grade = "B";
+  } else if (score > 15) {
+    grade = "C";
+  } else if (score > 10) {
+    grade = "D";
+  } else if (score > 5) {
+    grade = "E";
+  } else if (score >= 0) {
+    grade = "F";
+  }
 
-    return grade;
+  return grade;
 }
 
-
 function main() {
-    const score = +(readLine());
+  const score = +readLine();
 
-    console.log(getGrade(score));
+  console.log(getGrade(score));
 }
