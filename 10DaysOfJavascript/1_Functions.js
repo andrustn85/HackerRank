c42037fd2d6f59366971b9f72bdeea6f456f9d7b
sarah <sarahkharraz@gmail.com>
2020-05-28 18:01:05 +0100

    10D - refactoring

--- 10DaysOfJavascript/1_Functions.js
+++ 10DaysOfJavascript/1_Functions.js
@@ -1,73 +1,67 @@
-// Objective
-//
-// Today, we're discussing JavaScript functions. Check out the attached tutorial for more details.
-//
-// Task
-//
-// Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
-//
-// Input Format
-//
-// Locked stub code in the editor reads a single integer, , from stdin and passes it to a function named factorial.
-//
-// Constraints
-//
-// Output Format
-//
-// The function must return the value of .
-//
-// Sample Input 0
-//
-// 4
-// Sample Output 0
-//
-// 24
-// Explanation 0
-//
-// We return the value of .
+/*
 
+Objective
+Today, we're discussing JavaScript functions. Check out the attached tutorial for more details.
+
+Task
+Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
+
+Input Format
+Locked stub code in the editor reads a single integer, , from stdin and passes it to a function named factorial.
+
+Constraints
+Output Format
+The function must return the value of .
+
+Sample Input 0
+4
+Sample Output 0
+24
+Explanation 0
+We return the value of .
+
+*/
 
 // SOLUTION
 
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map(string => {
+      return string.trim();
     });
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
-/*
- * Create the function factorial here
- */
+
 const factorial = n => {
-    if (n < 0)
-        return -1;
-    else if (n === 0)
-        return 1;
-    else {
-        const res = (n * factorial(n - 1))
-        return res;
-    }
-}
+  if (n < 0) return -1;
+  else if (n === 0) return 1;
+  else {
+    const res = n * factorial(n - 1);
+    return res;
+  }
+};
 
 function main() {
-    const n = +(readLine());
+  const n = +readLine();
 
-    console.log(factorial(n));
+  console.log(factorial(n));
 }
