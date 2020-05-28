e1161c455633207b9f48ea439811cea1c498821d
sarah <sarahkharraz@gmail.com>
2020-05-28 16:23:16 +0100

    10D - Buttons container - JS

--- 10DaysOfJavascript/8_Buttons_Container.js
+++ 10DaysOfJavascript/8_Buttons_Container.js
@@ -65,3 +65,17 @@
 //     height: 48px;
 //     font-size: 24px;
 // }
+
+// In JS file :
+
+btn5.onclick = function() {
+  let temp = btn1.innerHTML;
+  btn1.innerHTML = btn4.innerHTML;
+  btn4.innerHTML = btn7.innerHTML;
+  btn7.innerHTML = btn8.innerHTML;
+  btn8.innerHTML = btn9.innerHTML;
+  btn9.innerHTML = btn6.innerHTML;
+  btn6.innerHTML = btn3.innerHTML;
+  btn3.innerHTML = btn2.innerHTML;
+  btn2.innerHTML = temp;
+};
