81381c811ea6521ce6665aa953e647dab9ef777a
sarah <sarahkharraz@gmail.com>
2020-05-28 17:55:17 +0100

    10D - refactoring

--- 10DaysOfJavascript/3_Arrays.js
+++ 10DaysOfJavascript/3_Arrays.js
@@ -1,86 +1,85 @@
-// Objective
-//
-// In this challenge, we learn about Arrays. Check out the attached tutorial for more details.
-//
-// Task
-//
-// Complete the getSecondLargest function in the editor below. It has one parameter: an array, , of  numbers. The function must find and return the second largest number in .
-//
-// Input Format
-//
-// Locked stub code in the editor reads the following input from stdin and passes it to the function:
-// The first line contains an integer, , denoting the size of the  array.
-// The second line contains  space-separated numbers describing the elements in .
-//
-// Constraints
-//
-// , where  is the number at index .
-// The numbers in  are not distinct.
-// Output Format
-//
-// Return the value of the second largest number in the  array.
-//
-// Sample Input 0
-//
-// 5
-// 2 3 6 6 5
-// Sample Output 0
-//
-// 5
-// Explanation 0
-//
-// Given the array , we see that the largest value in the array is  and the second largest value is . Thus, we return  as our answer.
+/*
 
+Objective
+In this challenge, we learn about Arrays. Check out the attached tutorial for more details.
+
+Task
+Complete the getSecondLargest function in the editor below. It has one parameter: an array, , of  numbers. The function must find and return the second largest number in .
+
+Input Format
+Locked stub code in the editor reads the following input from stdin and passes it to the function:
+The first line contains an integer, , denoting the size of the  array.
+The second line contains  space-separated numbers describing the elements in .
+
+Constraints
+, where  is the number at index .
+The numbers in  are not distinct.
+
+Output Format
+Return the value of the second largest number in the  array.
+
+Sample Input 0
+5
+2 3 6 6 5
+Sample Output 0
+5
+Explanation 0
+Given the array , we see that the largest value in the array is  and the second largest value is . Thus, we return  as our answer.
+
+*/
 
 // SOLUTION
 
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map(string => {
+      return string.trim();
     });
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /**
-*   Return the second largest number in the array.
-*   @param {Number[]} nums - An array of numbers.
-*   @return {Number} The second largest number in the array.
-**/
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+
 function getSecondLargest(nums) {
-    // Complete the function
+  const maxNum = Math.max(...nums);
+  const idx = nums.indexOf(maxNum);
+  const removeMax = nums.splice(idx, 1);
 
-    const maxNum = Math.max(...nums)
-    const idx = nums.indexOf(maxNum)
-    const removeMax = nums.splice(idx, 1)
+  const removeAgain = nums.splice(idx, 1);
+  const includes = nums.includes(maxNum) ? removeAgain : nums;
+  const secondBigger = Math.max(...nums);
 
-    const removeAgain = nums.splice(idx, 1)
-    const includes = nums.includes(maxNum)? removeAgain : nums
-    const secondBigger = Math.max(...nums)
-
-    return secondBigger
+  return secondBigger;
 }
 
-
 function main() {
-    const n = +(readLine());
-    const nums = readLine().split(' ').map(Number);
+  const n = +readLine();
+  const nums = readLine()
+    .split(" ")
+    .map(Number);
 
-    console.log(getSecondLargest(nums));
+  console.log(getSecondLargest(nums));
 }
