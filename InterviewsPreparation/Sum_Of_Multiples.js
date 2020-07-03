79a170b1d4667b0c62c3409674ef138864514dde
sarah <sarahkharraz@gmail.com>
2020-07-03 21:00:09 +0100

    Sum of multples - ex

---
+++ InterviewsPreparation/Sum_Of_Multiples.js
@@ -0,0 +1,31 @@
+/*
+
+INSTRUCTIONS
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Specification
+solution(number)
+Finds the sum of multiples of 3 or 5 that is less than the provided number
+
+Parameters:
+number: Number - Maximum number to check against
+
+Return Value
+Number - Sum of all multiples of either 3 or 5
+
+Examples:
+number	Return Value
+10	    23
+200	    9168
+
+Sample test:
+describe('Multiples of 3 and 5', function() {
+  it('should handle basics', function() {
+    Test.assertEquals(solution(10), 23);
+  });
+});
+
+*/
